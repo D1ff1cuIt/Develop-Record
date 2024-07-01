3908cad77dceafee58d7f14547f4000d4bba663e
baby361200 <baby361200@gmail.com>
2024-07-01 11:30:39 +0800

    上傳更改及新練習的部分

---
+++ C sharp Practice/四則運算 單純想練習Class運用/四則運算練習/Calculate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 四則運算練習
+{
+    internal class Calculate
+    {
+        public static int Plus(int A, int B)
+        {
+            return A + B;
+        }
+
+        public static int Minus(int A, int B)
+        {
+            return A - B;
+        }
+
+        public static double Times(Double A, Double B)
+        {
+            return A * B;
+        }
+
+        public static double Divided(Double A, Double B)
+        {
+            return A / B;
+        }
+    }
+}
---
+++ C sharp Practice/四則運算 單純想練習Class運用/四則運算練習/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 四則運算練習
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String A = textBox1.Text;
+            String B = textBox2.Text;
+            label5.Text = Convert.ToString(Calculate.Plus(Convert.ToInt32(A), Convert.ToInt32(B)));
+            label6.Text = Convert.ToString(Calculate.Minus(Convert.ToInt32(A), Convert.ToInt32(B)));
+            label7.Text = Convert.ToString(Calculate.Times(Convert.ToInt32(A), Convert.ToInt32(B)));
+            label8.Text = Convert.ToString(Calculate.Divided(Convert.ToInt32(A), Convert.ToInt32(B)));
+        }
+    }
+}
